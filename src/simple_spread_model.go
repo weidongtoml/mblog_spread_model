0b4a29845f00b37295d3bd3da198dd4ef842df1c
weidongtoml <weidongtom@gmail.com>
2013-07-17 18:05:58 +0800

    Add progress monitor

--- src/simple_spread_model.go
+++ src/simple_spread_model.go
@@ -18,13 +18,28 @@ func main() {
 	flag.Parse()
 
 	simulator := new(spread_model.Simulator)
+	
+	
+	fmt.Printf("Loading data from files [%s],[%s]..\n", *user_active_rate_file, *user_interaction_rate_file)
+	
 	simulator.LoadSpreadModelData(*user_active_rate_file, *user_interaction_rate_file)
+	
+	fmt.Printf("Done\n")
+	
 	parameters := simulator.GetParameters()
+	
+	//TODO(weidoliang): iterate througn different avg_retweet_rate and depth to produce 
+	//results under different parameters
 	parameters.Avg_retweet_rate = 0.05
 	parameters.Max_depth = 3
 	parameters.Is_random_sim = true
 	parameters.Random_sim_rounds = 100
 	
+	fmt.Printf("Runing simulation with Parameters: %v\n...", *parameters)
+	
 	result := simulator.RunSimulation()
+	
+	fmt.Printf("Done\n")
+		
 	fmt.Printf("Average Retweet Count: %f\n", result.GetAverageRetweetCount())
 }
